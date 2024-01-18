60569b31c01d7803e986158c39d59185dc95197e
Henri <henriaernoudts@gmail.com>
2024-01-18 11:58:11 -0300

    responsive grid

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 const body = document.querySelector("body");
-const block = document.createElement("block");
+const send = document.querySelector(".send");
+const block = document.querySelector(".block");
+const inputGridSize = document.querySelector(".inputGridSize");
 body.appendChild(block);
-let gridsize = 0
 
-function createGrid(gridsize) {
-    for (let i = 1; i <= gridsize; i++) {
+let gridsize = 0;
+
+send.addEventListener('click', function() {
+    if (inputGridSize.value < 100) {
+        var gridSize = inputGridSize.value;
+        createGrid(gridSize);
+    }
+});
+
+function changeColor(box) {
+    box.addEventListener('click', function() {
+            box.style.background = "black"
+    });
+}
+
+function createGrid(gridSize) {
+    for (let i = 1; i <= gridSize; i++) {
         const container = document.createElement("container")
         container.setAttribute("class", "container");
         block.appendChild(container);
-        for (let i = 1; i <= gridsize; i++) {
+        for (let i = 1; i <= gridSize; i++) {
             let box = document.createElement("box");
             box.setAttribute("class", "box");
             box.innerHTML = "";
+            changeColor(box);
             container.appendChild(box);
         }
-        container.appendChild(document.createElement("br"));
     }
 }
