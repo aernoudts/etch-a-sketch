5aa183cbcb1c8553baa64ea2c8ee529f3de68dc5
aernoudts <henriaernoudts@gmail.com>
2024-01-22 21:03:38 -0300

    added eraser

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ const body = document.querySelector("body");
 const send = document.querySelector(".send");
 const block = document.querySelector(".block");
 const inputGridSize = document.querySelector(".inputGridSize");
+const drawButton = document.querySelector("#draw")
+const eraseButton = document.querySelector("#erase")
+const startOver = document.querySelector(".startover")
+drawButton.style.background = "lightgray";
 body.appendChild(block);
 
 let gridsize = 0;
+let color = "black"
 
 send.addEventListener('click', function() {
-    if (inputGridSize.value <= 100) {
+    if (inputGridSize.value <= 60) {
         while (block.firstChild) {
             block.removeChild(block.firstChild);
             }
@@ -18,9 +23,23 @@ send.addEventListener('click', function() {
     }
 });
 
+drawButton.addEventListener('click', function() {
+    color = "black";
+    drawButton.style.background = "lightgray";
+    eraseButton.style.background = "";
+
+});
+
+eraseButton.addEventListener('click', function() {
+    color = "white";
+    eraseButton.style.background = "lightgray";
+    drawButton.style.background = "";
+});
+
+
 function changeColor(box) {
     box.addEventListener('click', function() {
-            box.style.background = "black"
+            box.style.background = color;
     });
 }
 
